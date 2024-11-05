412b9037892a17b8664a2eae954aff675081d54f
fkkdrive <fkkdrive@gmail.com>
2024-11-05 14:24:22 +0800

    initial commit

---
+++ pulltorefresh/lib/main.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ActivityImpl _$$ActivityImplFromJson(Map<String, dynamic> json) =>
+    _$ActivityImpl(
+      activity: json['activity'] as String,
+      type: json['type'] as String,
+      participants: (json['participants'] as num).toInt(),
+      price: (json['price'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$$ActivityImplToJson(_$ActivityImpl instance) =>
+    <String, dynamic>{
+      'activity': instance.activity,
+      'type': instance.type,
+      'participants': instance.participants,
+      'price': instance.price,
+    };
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$activityHash() => r'609ac1c1d8008d8109ea5869c7aa88013032917c';
+
+/// See also [activity].
+@ProviderFor(activity)
+final activityProvider = AutoDisposeFutureProvider<Activity>.internal(
+  activity,
+  name: r'activityProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$activityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ActivityRef = AutoDisposeFutureProviderRef<Activity>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
